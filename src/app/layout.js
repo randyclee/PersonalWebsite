e947a5314d1c454fefe9bbaf59743929e201eedd
Randy Lee <randylee@Randys-MacBook-Air.local>
2023-11-27 15:30:17 -0500

    added change to title of home

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "Randy Lee",
+  title: "Randy Lee - Software Engineer",
   description: 'Randy Lee is a software engineer based in Toronto, ON with a few years of experience in the industry. This website contains apps coded for you to have fun and also contains information about my achievements',
 }
 
